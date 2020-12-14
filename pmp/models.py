c555f817eccfc9fd2d67c668e51bccd1472eee87
AshveerB <ashveerbhayroo@gmail.com>
2020-12-14 09:20:10 -0800

    add models

--- pmp/models.py
+++ pmp/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Habits(models.Model):
+    water = models.IntegerField()
+    sleep = models.IntegerField()
+    exercise = models.IntegerField()
+    calories = models.IntegerField()
+    learning = models.IntegerField()
+    spending = models.IntegerField()
+    earning = models.IntegerField()
+    travel = models.IntegerField()
+    date = models.DateField(unique=True)
+
+
+class Goals(models.Model):
+    water = models.IntegerField()
+    sleep = models.IntegerField()
+    exercise = models.IntegerField()
+    calories = models.IntegerField()
+    learning = models.IntegerField()
+    spending = models.IntegerField()
+    earning = models.IntegerField()
+    travel = models.IntegerField()
+
+
+class Reflection(models.Model):
+    date = models.DateField(unique=True)
+    reflection = models.TextField()
