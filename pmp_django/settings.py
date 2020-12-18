4467ec8fc52179f7849734a197de10333f011948
AshveerB <ashveerbhayroo@gmail.com>
2020-12-18 05:44:13 -0800

    deploy app

--- pmp_django/settings.py
+++ pmp_django/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.environ['MODE'] == 'dev' else False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
